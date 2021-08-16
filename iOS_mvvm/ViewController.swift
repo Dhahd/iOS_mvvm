cc59452bed4b8dfeecb405b076d6a561080418a6
Shaneen Dhahd <hadidhahds@gmail.com>
2021-08-16 23:01:29 +0300

    Initial Commit

---
+++ iOS_mvvm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_mvvm
+//
+//  Created by Hadi Dhahd on 16/08/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
